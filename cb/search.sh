36404860ad4a9f964b41201d05dce9f6422343b0
Toan Phung <toan.phung@proteo.info>
2024-09-25 15:50:18 +0100

    Update search.sh

--- cb/search.sh
+++ cb/search.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 IFS=',' read -ra words <<< "$1"   # Split comma-separated words into an array
 for word in "${words[@]}"; do
-    grep -inE "(^|[^[:alnum:]_-\t,])((_|;|-)?($word)(_|;|-)?)(\b|[^[:alnum:]_-\t,])" "$2" | awk -v word="$word" '{print word ": " $0}'
+    grep -inE "(^|[^[:alnum:]_-])((_|;|-|\t)?($word)(_|;|-|\t)?)(\b|[^[:alnum:]_-])" "$2" | awk -v word="$word" '{print word ": " $0}'
     #grep -inE "(^|[^[:alnum:]_-])(($word)(_|;|-)?)(\b|[^[:alnum:]_-])" "$2" | awk -v word="$word" '{print word ": " $0}'      # Search for each word case-insensitively
     #grep -inE "(^|[^[:alnum:]_-])($word(_|-|\.| |;|\t|,|$))" "$2" | awk -v word="$word" '{print word ": " $0}'
 done
