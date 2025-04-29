ef03c66e7f36c11cc39e416a4791c953c2f2c714
Toan Phung <toan.phung@proteo.info>
2025-04-29 20:16:54 +0100

    Update viewsets.py

--- cb/viewsets.py
+++ cb/viewsets.py
@@ -65,10 +65,10 @@ class ProjectViewSet(viewsets.ModelViewSet, FilterMixin):
             query &= Q(user__lab_groups__id__in=lab_group.split(","))
         if users:
             query &= Q(user__id__in=users.split(","))
-        # check if the Project is public. If not, check if the user is authenticated and is the owner of the Project or the Project is shared_with the user.
+        # check if the Project is public. If not, check if the user is authenticated and is the owner of the Project or the Project is share_with the user.
         current_user = self.request.user
         if current_user.is_authenticated:
-            query &= Q(public=True) | Q(user=current_user) | Q(shared_with__in=current_user)
+            query &= Q(public=True) | Q(user=current_user) | Q(share_with__in=current_user)
         else:
             query &= Q(public=True)
 
@@ -209,9 +209,9 @@ class AnalysisGroupViewSet(viewsets.ModelViewSet, FilterMixin):
         if users:
             query &= Q(project__user__id__in=users.split(","))
 
-        # check if the Project of the AnalysisGroup is public. If not, check if the user is authenticated and is the owner of the Project or the Project is shared_with the user.
+        # check if the Project of the AnalysisGroup is public. If not, check if the user is authenticated and is the owner of the Project or the Project is share_with the user.
         if current_user.is_authenticated:
-            query &= Q(project__public=True) | Q(project__user=current_user) | Q(project__shared_with__in=current_user)
+            query &= Q(project__public=True) | Q(project__user=current_user) | Q(project__share_with__in=current_user)
         else:
             query &= Q(project__public=True)
 
